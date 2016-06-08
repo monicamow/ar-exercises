474bdd31d21f6bdf1655dfa89a2496717e72f74a
monicamow <mow.monica@gmail.com>
2016-06-08 14:09:33 -0700

    ex 5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.all.sum(:annual_revenue)
+p @total_revenue
+
+@average_revenue = Store.average(:annual_revenue).round
+p @average_revenue
+
+@big_sales = Store.where("annual_revenue > ?", 1_000_000).count
+p @big_sales
+
